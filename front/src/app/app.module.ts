6b9d589742cca01c7cd4abaa3ac0705c7ed53751
Cocoricodai <colin.depelsenaire@hotmail.com>
2024-05-20 17:24:18 +0200

    (BM-5) - Initialize routes factory

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,17 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {AngularSvgIconModule} from "angular-svg-icon";
 import {HttpClientModule} from "@angular/common/http";
+import {RouterOutlet, ROUTES} from "@angular/router";
+import {FactoriesUtils} from "./core/utils/factories/factories.utils";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+    declarations: [
+        AppComponent,
+    ],
     imports: [
         AngularSvgIconModule.forRoot(),
         HttpClientModule,
         BrowserModule,
+        RouterOutlet,
     ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [
+        {
+            provide: ROUTES,
+            useFactory: FactoriesUtils.routesFactory,
+            deps: [],
+            multi: true,
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ front/src/app/core/utils/factories/factories.utils.ts
@@ -0,0 +1,9 @@
+import {Routes} from "@angular/router";
+
+export namespace FactoriesUtils {
+
+    // Routes Factory
+    export function routesFactory(): Routes[] {
+        return []
+    }
+}
